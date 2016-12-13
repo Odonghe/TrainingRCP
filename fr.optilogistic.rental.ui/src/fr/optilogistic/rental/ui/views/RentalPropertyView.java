2ce01e4fba246231270b30fc151fe717ed180b7d
againa <againa@VMDEV03.optilogistic.dom>
2016-12-13 09:50:48 +0100

    Create new group

--- fr.optilogistic.rental.ui/src/fr/optilogistic/rental/ui/views/RentalPropertyView.java
+++ fr.optilogistic.rental.ui/src/fr/optilogistic/rental/ui/views/RentalPropertyView.java
@@ -1,5 +1,9 @@
 package fr.optilogistic.rental.ui.views;
 
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
@@ -17,6 +21,8 @@ public class RentalPropertyView extends ViewPart {
 
 	private Label rentedObjectLabel;
 	private Label rentedToLabel;
+	private Label lblDateRentalStart;
+	private Label lblDateRentalEnd;
 	
 	public RentalPropertyView() {
 		// TODO Auto-generated constructor stub
@@ -26,6 +32,15 @@ public class RentalPropertyView extends ViewPart {
 		if(rental != null) {
 			rentedObjectLabel.setText(rental.getRentedObject() != null ? rental.getRentedObject().getName() : "");
 			rentedToLabel.setText(rental.getCustomer() != null ? rental.getCustomer().getFirstName() + " " + rental.getCustomer().getLastName() : "");
+			SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+			Random rand = new Random();
+			char[] charSequence = new char[2000];
+			for(int i = 0; i < 2000; i++) {
+				charSequence[i] = (char) (65 + rand.nextInt(28)); 
+			}
+			String longString = new String(charSequence);
+			lblDateRentalStart.setText(dateFormat.format(rental.getStartDate()) + longString);
+			lblDateRentalEnd.setText(dateFormat.format(rental.getEndDate()) + longString);
 		}
 	}
 	
@@ -34,8 +49,9 @@ public class RentalPropertyView extends ViewPart {
 		parent.setLayout(new GridLayout(1,false));
 		
 		Group infoGroup = new Group(parent, SWT.NONE);
+		infoGroup.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
 		infoGroup.setText("Informations");
-		infoGroup.setLayout(new GridLayout(1, false));
+		infoGroup.setLayout(new GridLayout(2, false));
 		
 		
 		rentedObjectLabel = new Label(infoGroup, SWT.NONE);
@@ -49,6 +65,26 @@ public class RentalPropertyView extends ViewPart {
 		
 		rentedToLabel = new Label(infoGroup, SWT.NONE);
 		
+		Group grpDatesDeLocation = new Group(parent, SWT.NONE);
+		grpDatesDeLocation.setLayout(new GridLayout(2, false));
+		GridData gd_grpDatesDeLocation = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1);
+		gd_grpDatesDeLocation.widthHint = 128;
+		gd_grpDatesDeLocation.heightHint = 69;
+		grpDatesDeLocation.setLayoutData(gd_grpDatesDeLocation);
+		grpDatesDeLocation.setText("Dates de location");
+		
+		Label lblDu = new Label(grpDatesDeLocation, SWT.NONE);
+		lblDu.setText("du: ");
+		
+		lblDateRentalStart = new Label(grpDatesDeLocation, SWT.NONE);
+		lblDateRentalStart.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Label lblAu = new Label(grpDatesDeLocation, SWT.NONE);
+		lblAu.setText("au: ");
+		
+		lblDateRentalEnd = new Label(grpDatesDeLocation, SWT.NONE);
+		lblDateRentalEnd.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
 		setRental(createSampleRental());
 	}
 
@@ -60,5 +96,4 @@ public class RentalPropertyView extends ViewPart {
 	@Override
 	public void setFocus() {
 	}
-
 }
